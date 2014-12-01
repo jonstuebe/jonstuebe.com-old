6528ea2810a0c61c13c250d408407d4d5c554b9c
Jonathan Stuebe <jstuebe@gmail.com>
2014-11-30 23:42:35 -0700

    basics of blog layout started

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,23 @@
-var Anchor = {
-	init: function() {
-		Anchor.slidey = $('.slidey');
-		
-		//  Uh, bind to the resizing of the window?
-		$(window).resize(Anchor.bindResize).trigger('resize');
-		
-		//  Set up the toggle link
-		Anchor.linky = $('.linky').click(Anchor.toggleSlidey);
+//----------------------doc ready-------------------------------------------------------------
+$(document).ready(function(){
 
-		//  Hide the thingymabob
-		setTimeout(function() {
-		    //  Set up the slidey panel
-		    Anchor.hideSlidey();
-		    
-			$('body').addClass('js-enabled');
-		}, 10);
-		
-		//  Listen for search link
-		$('a[href="#search"]').click(function() {
-			if(!Anchor.linky.hasClass('active')) {
-				return Anchor.toggleSlidey.call(Anchor.linky);
-			}
-		});
-	},
-	
-	hideSlidey: function() {
-		Anchor.slidey.css('margin-top', this._slideyHeight);
-		Anchor.linky && Anchor.linky.removeClass('active');
-		
-		return this;
-	},
-	
-	toggleSlidey: function() {
-		var self = Anchor;
-		var me = $(this);
+	var $doc = $(document).last,
+		$win = $(window);
+
+	var	g = {
 			
-		me.toggleClass('active');
-		self.slidey.css('margin-top', me.hasClass('active') ? 0 : self._slideyHeight);
-		
-		return false;
-	},
-	
-	bindResize: function() {
-		Anchor._slideyHeight = -(Anchor.slidey.height() + 1);
-		Anchor.hideSlidey();
-	}
-};
 
-//  And bind loading
-$(Anchor.init);
+	},//general
+
+	n = {
+		initNavigation: function(){
+
+		}
+	},//navigation
+
+	l = {
+
+		
+	}; //loader
+
+});//----------------------doc ready-------------------------------------------------------------
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp'),
+	notify = require('gulp-notify'),
+	minifyCSS = require('gulp-minify-css'),
+	changed = require('gulp-changed'),
+	rename = require("gulp-rename"),
+	sourcemaps   = require('gulp-sourcemaps'),
+	autoprefixer = require('gulp-autoprefixer'),
+	sass = require('gulp-sass'),
+	bourbon = require('node-bourbon'),
+	neat = require('node-neat');
+
+var paths = {
+	scripts: './assets/js/**/*.js',
+	styles: './assets/sass/**/*.scss',
+	output: {
+		styles: './assets/css'
+	}
+}
+
+gulp.task('styles.prod', function(){
+
+	gulp.src(paths.styles)
+    	.pipe(changed(paths.output.styles, { extension: '.css' }))
+    	.pipe(sass({
+    		includePaths: neat.includePaths
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions']
+        }))
+        .pipe(minifyCSS({
+        	keepSpecialComments: 0
+        }))
+        .pipe(rename({
+        	extname: '.min.css'
+        }))
+        .pipe(notify("production sass compiled"))
+        .pipe(gulp.dest(paths.output.styles));
+
+});
+
+gulp.task('styles', function()
+{
+    gulp.src(paths.styles)
+    	// .pipe(changed(paths.output.styles, { extension: '.css' }))
+    	.pipe(sourcemaps.init())
+    	.pipe(sass({
+    		includePaths: neat.includePaths,
+    		sourceMap: true
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(notify("sass compiled"))
+        .pipe(gulp.dest(paths.output.styles));
+});
+
+gulp.task('watch', function()
+{
+	gulp.watch(paths.styles, ['styles']);
+});
+
+gulp.task('default', ['styles', 'watch']);
+gulp.task('prod', ['styles.prod']);
