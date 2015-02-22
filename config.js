71d4bc67c8fbd0ccb3c4ed7f755eb575b6da5575
Jon Stuebe <jstuebe@gmail.com>
2015-02-21 17:52:15 -0700

    updated with disqus comments

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blogs published URL.
-        url: 'http://jonstuebe.com',
+        url: 'http://localhost:2368',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
